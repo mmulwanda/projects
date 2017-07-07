4c9c2b8ec9d995cf27976fd8e6ea14e8f912793b
mmulwanda <mamaimulwanda@gmail.com>
2017-07-07 15:08:09 -0400

    Add files via upload

---
+++ textadventure.py
@@ -0,0 +1,58 @@
+print('''You outside the metro on your way to Girls Who Code SIP, when you get a text
+from a friend to hangout with them and others on U Street.''')
+
+print("Type 'GWC' to go GWC or 'U Street' to go U Street.")
+user_input = input()
+
+if user_input == "GWC":
+
+    print('''You are late and rushing and you drop coffee on your white shirt''')
+
+    print("Type 'Street' to buy s shirt on the street or 'GWC' to go to GWC:")
+    user_input2 = input()
+
+    if user_input2 == "Street":
+
+        print('''The only shirt available on the street are Trump shirts''')
+
+        print("Type 'Buy' to buy the Trump shirt or 'Walk' to not but the shirt")
+        user_input3 = input()
+
+        if user_input3 == "Buy":
+
+            print('''You turn the shirt insideout because you do not agree with him and are embarrassed, which leads to people giving you weird looks as you walk.''')
+
+        if user_input3 == "Walk":
+
+            print('''You do not believe the shirt is worth your money and go to class with a coffee stain on your clothes.''') # finished the story writing what happens
+
+    if user_input2 == "GWC":
+
+        print('''When you arrive to the Georgetown building Maya sees you and the dilema you are in. Since she has such a kind heart she excuses you for the day and you end up going home for the day. After you have cleaned your self up you meet up with friend on U Street to have brunch.''') # finished the story by writing what happens
+
+if user_input == "U Street":
+
+         print(''' Maya emails your parents and your parents text you''')
+
+         print("Type 'GWC' to go back to GWC or 'Lie' to lie to your parents")
+         user_input4 = input()
+
+         if user_input4 == "GWC":
+
+             print('''On your way you trip and rip your jeans, which leads your knee to bleed.''')
+
+             print("Type 'Keep going' to just ignore it and keep going or 'CVS' to go to CVS to buy band-aids")
+             user_input5 = input()
+
+             if user_input5 == "Keep going":
+
+                print('''You limp on the the way to CWC Georgetown and you get to the class to find out it is cancled''')
+
+             if user_input5 == "CVS":
+
+                print('''You bought band-aids,got your wound cleaned, and began to walk to GWC. When get there you have a great day and go home.''' ) # finished the story writing what happens
+
+
+         if user_input4 == "Lie":
+
+            print('''Spend your day with your friends on U Street and your parents never find out.''') # finished the story writing what happens
